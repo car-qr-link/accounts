746f40ee6ef1b43b39ff7b76cf9576f220d5d588
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-11-15 06:53:39 +0700

    [init] apply template

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+import { DataSourceOptions } from 'typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { dataSourceOptions } from './db';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        // install 'pino-pretty' package in order to use the following option
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty', options: { colorize: true } }
+            : undefined,
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () =>
+        ({
+          ...dataSourceOptions,
+          type: new URL(
+            process.env.DATABASE_URL || dataSourceOptions.url,
+          ).protocol.replaceAll(':', ''),
+          url: process.env.DATABASE_URL,
+
+          synchronize: process.env.NODE_ENV !== 'production',
+          migrationsRun: process.env.NODE_ENV === 'production',
+        }) as unknown as DataSourceOptions,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
