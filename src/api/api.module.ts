4576448e176ca79161d924fad038e7eb20696ca4
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-11-15 07:01:20 +0700

    [api] add controllers

---
+++ src/api/api.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AccountsModule } from './accounts/accounts.module';
+import { QrsModule } from './qrs/qrs.module';
+
+@Module({
+  imports: [AccountsModule, QrsModule]
+})
+export class ApiModule {}
